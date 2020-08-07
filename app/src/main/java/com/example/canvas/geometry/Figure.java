361d56dbb9a708647c005d8ed99249fb29800c13
Max <49816300+Grand-Max@users.noreply.github.com>
2020-08-07 15:54:08 +0300

    basepoint init

--- app/src/main/java/com/example/canvas/geometry/Figure.java
+++ app/src/main/java/com/example/canvas/geometry/Figure.java
@@ -1,12 +1,25 @@
 package com.example.canvas.geometry;
 
-public abstract class Figure extends Point {
+public abstract class Figure {
 
     private int offsetX, offsetY;
+    private Point p;
+
+    public Figure (){
+        p = new Point();
+    }
+
+    public Point getP() {
+        return p;
+    }
+
+    public void setP(Point p) {
+        this.p = p;
+    }
 
     public void offset(){
-        setX(getX() + offsetX);
-        setY(getY() + offsetY);
+        p.setX(p.getX() + offsetX);
+        p.setY(p.getY() + offsetY);
     }
 
     public int getOffsetX() {
@@ -23,6 +36,22 @@ public abstract class Figure extends Point {
 
     public void setOffsetY(int offsetY) {
         this.offsetY = offsetY;
+    }
+
+    public int getX() {
+        return p.getX();
+    }
+
+    public void setX(int x) {
+        this.p.setX(x);
+    }
+
+    public int getY() {
+        return p.getY();
+    }
+
+    public void setY(int y) {
+        this.p.setY(y);
     }
 
 }
